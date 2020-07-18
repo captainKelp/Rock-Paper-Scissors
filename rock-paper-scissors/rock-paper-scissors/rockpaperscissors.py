4452bca7accdda94b3109fca03ddcd28435b1f3e
captainKelp <rehman.alveena@gmail.com>
2020-07-18 17:29:38 -0500

    I got the buttons and labels organized I don't know how pad works I don't know how to code the rest of this game var outcome is apparently nonexistent

--- rock-paper-scissors/rock-paper-scissors/rockpaperscissors.py
+++ rock-paper-scissors/rock-paper-scissors/rockpaperscissors.py
@@ -4,7 +4,6 @@ import tkinter
 #NOTE: A function that determines whether the user wins or not
 #      Passes the user's choice (based on what button they click)to the parameter
 
-
 def get_winner(call):
     if event == "<rock":
         userChoice = "rock"
@@ -27,23 +26,28 @@ def get_winner(call):
     if computerChoice == 3:
         computerChoice = "scissors"
     return computerChoice
+
     # 3. Determine the winner based on what the user chose and what the computer chose
     root = tkinter()
 
-
-
     #   Rock beats Scissors
-    if (event == "rock") & (computerChoice == "scissors"):
-        win = win + 1
+    if (userChoice == "rock") & (computerChoice == "scissors"):
+        win += 1
         outcome = "you win"
-    else:
-        outcome = "you lose"
     #   Paper beats Rock
-
+    if (userChoice == "paper") & (computerChoice == "rock"):
+        win += 1
+        outcome = "you win"
     #   Scissors beats Paper
-
+    if (userChoice == "scissors") & (computerChoice == "paper"):
+        win += 1
+        outcome = "you win"
     #   It's a tie if the computer and user chose the same object
-
+    if (userChoice == computerChoice):
+        outcome = "you tied"
+    else:
+        output = "you lose"
+    return outcome
 
     # If the user wins, increase win by 1
     # Use the output label to write what the computer did and what the result was (win, loss, tie)
@@ -90,10 +94,7 @@ bg = "grey",
 fg = "white",
 )
 
-rock.pack()
-paper.pack()
-scissors.pack()
-
+#Allows buttons to function
 def callback(event):
     print("clicked at", event.x, event.y)
 
@@ -102,11 +103,40 @@ paper.bind("<Button-1>", callback)
 scissors.bind("<Button-1>", callback)
 
 # 2. Create 2 labels for the result and the number of wins
-stats = tkinter.Label(text = "result: " )
+stats = tkinter.Label(text = "result: ")
 score = tkinter.Label(text = "score: " + str(win))
 
-stats.pack()
-score.pack()
+
 # 3. Arrange the buttons and labels using grid
+
+#columns
+
+frame = tkinter.Frame(
+    master = window,
+    relief = tkinter.RAISED,
+    borderwidth = 1
+)
+
+frame.grid(row = 0, column = 1, padx = 5, pady = 5)
+
+buttons = tkinter.Button(master = frame)
+
+#window.columnconfigure(1, weight = 1, minsize = 25)
+#window.columnconfigure(2, weight = 1, minsize = 25)
+#window.columnconfigure(3, weight = 1, minsize = 25)
+
+#rows
+#window.rowconfigure(0, weight = 1, minsize = 25)
+#window.rowconfigure(1, weight = 1, minsize = 25)
+
+#buttons
+#rock.grid(row = 1, column = 1)
+#paper.grid(row = 1, column = 2)
+#scissors.grid(row = 1, column = 3)
+
+#labels
+#stats.grid(row = 0, column = 2)
+#score.grid(row = 0, column = 3)
+
 
 window.mainloop()
